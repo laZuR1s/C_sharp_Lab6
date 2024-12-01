a92edd08e3a2f7bb6538f39256f0e9fb2d8a14e4
Александр <sashalazyr@gmail.com>
2024-12-02 00:06:59 +0300

    add some features

--- C_sharp_Lab6/Form1.Designer.cs
+++ C_sharp_Lab6/Form1.Designer.cs
@@ -37,6 +37,9 @@
             button1 = new Button();
             textBox2 = new TextBox();
             button2 = new Button();
+            buttonSavePrevious = new Button();
+            buttonSaveResult = new Button();
+            buttonLoadFromFile = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
@@ -65,17 +68,23 @@
             dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
             dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            dataGridView1.Location = new Point(12, 62);
+            dataGridView1.Location = new Point(12, 113);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(293, 376);
+            dataGridView1.Size = new Size(293, 325);
             dataGridView1.TabIndex = 0;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 33);
+            textBox1.BackColor = Color.WhiteSmoke;
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Location = new Point(12, 12);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 1;
+            textBox1.Text = "Введите данные...";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.Enter += textBox1_Enter;
+            textBox1.Leave += textBox1_Leave;
             // 
             // dataGridView2
             // 
@@ -83,7 +92,7 @@
             dataGridView2.AllowUserToDeleteRows = false;
             dataGridView2.BackgroundColor = Color.White;
             dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Location = new Point(495, 62);
+            dataGridView2.Location = new Point(495, 113);
             dataGridView2.Name = "dataGridView2";
             dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = SystemColors.ButtonFace;
@@ -93,41 +102,87 @@
             dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
             dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            dataGridView2.Size = new Size(293, 376);
+            dataGridView2.Size = new Size(293, 325);
             dataGridView2.TabIndex = 2;
             // 
             // button1
             // 
-            button1.Location = new Point(347, 166);
+            button1.BackColor = Color.FromArgb(192, 192, 255);
+            button1.Location = new Point(348, 230);
             button1.Name = "button1";
             button1.Size = new Size(97, 76);
             button1.TabIndex = 3;
             button1.Text = "Изменить матрицу";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(347, 137);
+            textBox2.BackColor = Color.WhiteSmoke;
+            textBox2.ForeColor = Color.Gray;
+            textBox2.Location = new Point(348, 201);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(97, 23);
             textBox2.TabIndex = 4;
+            textBox2.Text = "Введите данные...";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.UseWaitCursor = true;
+            textBox2.Enter += textBox2_Enter;
+            textBox2.Leave += textBox2_Leave;
             // 
             // button2
             // 
-            button2.Location = new Point(146, 33);
+            button2.BackColor = Color.FromArgb(192, 255, 255);
+            button2.Location = new Point(143, 12);
             button2.Name = "button2";
             button2.Size = new Size(138, 23);
             button2.TabIndex = 5;
             button2.Text = "Создать матрицу";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
+            // 
+            // buttonSavePrevious
+            // 
+            buttonSavePrevious.BackColor = Color.FromArgb(192, 255, 192);
+            buttonSavePrevious.Location = new Point(86, 84);
+            buttonSavePrevious.Name = "buttonSavePrevious";
+            buttonSavePrevious.Size = new Size(125, 23);
+            buttonSavePrevious.TabIndex = 6;
+            buttonSavePrevious.Text = "Сохранить";
+            buttonSavePrevious.UseVisualStyleBackColor = false;
+            buttonSavePrevious.Click += buttonSavePrevious_Click;
+            // 
+            // buttonSaveResult
+            // 
+            buttonSaveResult.BackColor = Color.FromArgb(192, 255, 192);
+            buttonSaveResult.Location = new Point(574, 84);
+            buttonSaveResult.Name = "buttonSaveResult";
+            buttonSaveResult.Size = new Size(125, 23);
+            buttonSaveResult.TabIndex = 7;
+            buttonSaveResult.Text = "Сохранить";
+            buttonSaveResult.UseVisualStyleBackColor = false;
+            buttonSaveResult.Click += buttonSaveResult_Click;
+            // 
+            // buttonLoadFromFile
+            // 
+            buttonLoadFromFile.BackColor = Color.FromArgb(255, 192, 192);
+            buttonLoadFromFile.Location = new Point(306, 12);
+            buttonLoadFromFile.Name = "buttonLoadFromFile";
+            buttonLoadFromFile.Size = new Size(182, 23);
+            buttonLoadFromFile.TabIndex = 8;
+            buttonLoadFromFile.Text = "Заполнить из файла";
+            buttonLoadFromFile.UseVisualStyleBackColor = false;
+            buttonLoadFromFile.Click += buttonLoadFromFile_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
             ClientSize = new Size(800, 450);
+            Controls.Add(buttonLoadFromFile);
+            Controls.Add(buttonSaveResult);
+            Controls.Add(buttonSavePrevious);
             Controls.Add(button2);
             Controls.Add(textBox2);
             Controls.Add(button1);
@@ -150,5 +205,8 @@
         private Button button1;
         private TextBox textBox2;
         private Button button2;
+        private Button buttonSavePrevious;
+        private Button buttonSaveResult;
+        private Button buttonLoadFromFile;
     }
 }
--- C_sharp_Lab6/Form1.cs
+++ C_sharp_Lab6/Form1.cs
@@ -1,4 +1,6 @@
 using System.Data;
+using System.Drawing;
+using System.Security.Policy;
 
 namespace C_sharp_Lab6
 {
@@ -6,14 +8,13 @@ namespace C_sharp_Lab6
     {
 
         int N;
-
         public Form1()
         {
             InitializeComponent();
             dataGridView2.Visible = false;
             dataGridView1.Visible = false;
             this.Text = "Task 10";
-
+            this.Select();
         }
 
 
@@ -66,6 +67,155 @@ namespace C_sharp_Lab6
             for (i = 0; i < N; i++)
                 dataGridView1.Columns[i].Width = 50;
             dataGridView1.Visible = true;
+        }
+
+
+
+        private void buttonSavePrevious_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount > 0)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt|C# Files (*.cs)|*.cs|All Files (*.*)|*.*";
+                saveFileDialog.Title = "Сохранить исходную матрицу";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                    {
+                        int colCount = dataGridView1.ColumnCount;
+
+                        writer.WriteLine($"{colCount}");
+                        for (int i = 0; i < N; i++)
+                        {
+                            for (int j = 0; j < N; j++)
+                            {
+                                writer.Write(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                                if (j < N - 1)
+                                    writer.Write(' ');
+                            }
+                            writer.WriteLine();
+                        }
+                    }
+                    MessageBox.Show("Исходная матрица сохранена.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Невозможно сохранить матрицу");
+            }
+        }
+
+        private void buttonLoadFromFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|C# Files (*.cs)|*.cs|All Files (*.*)|*.*";
+            openFileDialog.Title = "Открыть файл";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] lines = File.ReadAllLines(openFileDialog.FileName);
+
+                N = Convert.ToInt32(lines[0]);
+
+                DataTable matr = new DataTable("matr");
+                DataColumn[] cols = new DataColumn[N];
+                for (int i = 0; i < N; i++)
+                {
+                    cols[i] = new DataColumn(i.ToString());
+                    matr.Columns.Add(cols[i]);
+                }
+                for (int i = 1; i <= N; i++)
+                {
+                    string[] values = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    DataRow newRow;
+                    newRow = matr.NewRow();
+                    for (int j = 0; j < N; j++)
+                    {
+                        newRow[j] = int.Parse(values[j]);
+                    }
+                    matr.Rows.Add(newRow);
+                }
+                dataGridView1.DataSource = matr;
+                dataGridView1.Visible = true;
+
+                for (int i = 0; i < N; i++)
+                    dataGridView1.Columns[i].Width = 50;
+                ;
+            }
+        }
+
+        private void buttonSaveResult_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount > 0)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt|C# Files (*.cs)|*.cs|All Files (*.*)|*.*";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                    {
+                        int colCount = dataGridView2.ColumnCount;
+                        writer.WriteLine($"{colCount}");
+                        for (int i = 0; i < N; i++)
+                        {
+                            for (int j = 0; j < colCount; j++)
+                            {
+                                writer.Write(dataGridView2.Rows[i].Cells[j].Value.ToString());
+                                if (j < colCount - 1)
+                                    writer.Write(' ');
+                            }
+                            writer.WriteLine();
+                        }
+                    }
+                    MessageBox.Show("Полученная матрица сохранена.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Невозможно сохранить матрицу");
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1 != null && textBox1.Text == "Введите данные...") 
+            {
+                textBox1.Text = "";  
+                textBox1.ForeColor = Color.Black;  
+            }
+            
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+
+            if ( string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Text = "Введите данные...";  
+                textBox1.ForeColor = Color.Gray; 
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2 != null && textBox2.Text == "Введите данные...") 
+            {
+                textBox2.Text = ""; 
+                textBox2.ForeColor = Color.Black; 
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if ( string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                textBox2.Text = "Введите данные...";  
+                textBox2.ForeColor = Color.Gray;  
+            }
+           
         }
     }
 }
